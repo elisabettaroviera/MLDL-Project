3ece8fd1d4901da41f9175671bba892bd466ac07
etta1509 <s328422@studenti.polito.it>
2025-04-29 22:33:38 +0200

    transform_datasets comments

--- datasets/transform_datasets.py
+++ datasets/transform_datasets.py
@@ -3,5 +3,7 @@ import torchvision.transforms as transforms
 # Define transformations for the Cityscapes
 # Add here any specific transformations you want to apply to the Cityscapes dataset
 def transform_cityscapes():
+    # NOTE: The training resolution and the test resolution are equal in Cityscapes
+    # Hence, we can use the same transform for both train and test
     transform = transforms.Resize((1024, 512))  # Resize to 1024x512
     return transform
