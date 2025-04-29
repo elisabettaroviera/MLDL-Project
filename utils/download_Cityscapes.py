6b821eb35ba2445f79d83581580c3c4802bc50dd
etta1509 <s328422@studenti.polito.it>
2025-04-29 16:54:10 +0200

    cityscapes download datasets

--- utils/download_Cityscapes.py
+++ utils/download_Cityscapes.py
@@ -2,16 +2,23 @@
 import requests
 from zipfile import ZipFile
 from io import BytesIO
+import gdown
 
 # Define the path to the dataset
-cityscapes_path = 'https://drive.google.com/file/d/1Qb4UrNsjvlU-wEsR9d7rckB0YS_LXgb2/view?usp=sharing'  
+cityscapes_id = '1Qb4UrNsjvlU-wEsR9d7rckB0YS_LXgb2'  # Google Drive file ID
+# cityscapes_path = 'https://drive.google.com/file/d/1Qb4UrNsjvlU-wEsR9d7rckB0YS_LXgb2/view?usp=sharing'  
+cityscapes_url = f'https://drive.google.com/uc?id={cityscapes_id}'
+# output = './datasets/cityscapes.zip'
+output = 'cityscapes.zip' # Name of the output file
+
+gdown.download(cityscapes_url, output, quiet=False)
 
 # Send a GET request to the URL
-response = requests.get(cityscapes_path)
+# response = requests.get(cityscapes_path)
 
 # Check if the request was successful
-if response.status_code == 200:
-    # Open the downloaded bytes and extract them
-    with ZipFile(BytesIO(response.content)) as zip_file:
-        zip_file.extractall('./datasets')
-    print('Download of the dataset Cityscapes and extraction complete!')
+# if response.status_code == 200:
+# Open the downloaded bytes and extract them
+with ZipFile("cityscapes.zip", "r") as zip_ref:
+    zip_ref.extractall('./datasets')
+print('Download of the dataset Cityscapes and extraction complete!')
