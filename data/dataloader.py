99d4b4b540a95468f9611915cac1c3b887149cda
etta1509 <s328422@studenti.polito.it>
2025-04-30 17:02:54 +0200

    dataloader

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,8 +1,10 @@
+# We have to do something else?
+# We have to move to datasets folder?
+
 from torch.utils.data import DataLoader
 
 # Create a DataLoader
-def dataloader(tiny_imagenet_dataset_train, tiny_imagenet_dataset_test):
-    dataloader_train = DataLoader(tiny_imagenet_dataset_train, batch_size=64, shuffle=True)
-    dataloader_test = DataLoader(tiny_imagenet_dataset_test, batch_size=64, shuffle=True)
-
-    return dataloader_train, dataloader_test
+def dataloader(dataset_train, dataset_val, batch_size, shuffle_train=True, shuffle_val=True):
+    dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=shuffle_train)
+    dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=shuffle_val)
+    return dataloader_train, dataloader_val
