ecc517601fd95f64f06767db5ff439612d5efa49
etta1509 <s328422@studenti.polito.it>
2025-04-30 17:03:36 +0200

    add to tensor

--- datasets/transform_datasets.py
+++ datasets/transform_datasets.py
@@ -5,5 +5,8 @@ import torchvision.transforms as transforms
 def transform_cityscapes():
     # NOTE: The training resolution and the test resolution are equal in Cityscapes
     # Hence, we can use the same transform for both train and test
-    transform = transforms.Resize((1024, 512))  # Resize to 1024x512
+    transform = transforms.Compose([
+                                    transforms.Resize((1024, 512)), # Resize to 1024x512
+                                    transforms.ToTensor() # Convert to tensor
+                                    ])
     return transform
