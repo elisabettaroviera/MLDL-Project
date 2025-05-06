a1e467894327be3dfa87784c615d23220c02e659
etta1509 <s328422@studenti.polito.it>
2025-05-06 16:55:52 +0200

    lr function

--- utils/utils.py
+++ utils/utils.py
@@ -29,6 +29,7 @@ def fast_hist(a, b, n):
     return np.bincount(n * a[k].astype(int) + b[k], minlength=n ** 2).reshape(n, n)
 
 
+# We implement our own function to compute the mean IoU
 def per_class_iou(hist):
     epsilon = 1e-5
     return (np.diag(hist)) / (hist.sum(1) + hist.sum(0) - np.diag(hist) + epsilon)
