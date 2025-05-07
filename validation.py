5a6247b2cdab1a49667e31fc24b8e8ff0acda94b
etta1509 <s328422@studenti.polito.it>
2025-05-07 16:56:11 +0200

    save image function

--- validation.py
+++ validation.py
@@ -13,6 +13,37 @@ from fvcore.nn import FlopCountAnalysis, flop_count_table
 import torchvision.transforms.functional as TF
 from datasets.cityscapes import CityScapes
 from utils.metrics import compute_miou, compute_latency_and_fps, compute_flops, compute_parameters
+from PIL import Image
+
+#Function to save sample images,ground truth color masks, prediction color masks
+def save_images(flag_save, save_dir,inputs, file_names, preds, color_targets,file_name_1, file_name_2):
+    # color map       
+    CITYSCAPES_COLORMAP = np.array([
+        [128, 64,128], [244, 35,232], [ 70, 70, 70], [102,102,156], [190,153,153],
+        [153,153,153], [250,170, 30], [220,220,  0], [107,142, 35], [152,251,152],
+        [ 70,130,180], [220, 20, 60], [255,  0,  0], [  0,  0,142], [  0,  0, 70],
+        [  0, 60,100], [  0, 80,100], [  0,  0,230], [119, 11, 32]
+    ], dtype=np.uint8)
+    
+    for input, file_name, pred, color_target in zip(inputs, file_names, preds, color_targets):
+        if file_name in [file_name_1, file_name_2]:
+            flag_save += 1
+
+            # Salva l'immagine originale da 'inputs' (tensore)
+            original_img = input.cpu().numpy().transpose(1, 2, 0)  # Converte (C, H, W) in (H, W, C)
+            original_img = np.clip(original_img * 255, 0, 255).astype(np.uint8)  # Normalizza tra 0-255
+            original_img_pil = Image.fromarray(original_img)
+            original_img_pil.save(f"{save_dir}/{file_name}_original.png")
+
+            # Salva la maschera predetta colorata
+            color_mask = CITYSCAPES_COLORMAP[pred]
+            color_mask_img = Image.fromarray(color_mask)
+            color_mask_img.save(f"{save_dir}/{file_name}_pred_color.png")
+
+            # Salva la maschera target colorata
+            if color_target is not None:
+                color_target_img = Image.fromarray(color_target)
+                color_target_img.save(f"{save_dir}/{file_name}_color_target.png")
 
 # VALIDATION LOOP
 def validate(epoch, new_model, val_loader, criterion, num_classes):
@@ -32,10 +63,24 @@ def validate(epoch, new_model, val_loader, criterion, num_classes):
     model.eval()
 
     print(f"Training on {len(val_loader)} batches")
+            # Color map Cityscapes to visualize the mask with colors
+
+    # Make sure the cartella outputs exists
+    save_dir = './outputs'
+    os.makedirs(save_dir, exist_ok=True)
+    # flag used to say whether we have saved the two pictures
+    # if flag = 1, we still need to save another picture
+    flag_save = 0
+
+    # image which we want to save the predicted masks of
+    file_name_1 = "frankfurt_000001_054640_leftImg8bit.png"
+    #frankfurt_000001_054640_gtFine_color.png
+    file_name_2 = "frankfurt_000001_062016_leftImg8bit.png"
+    #frankfurt_000001_062016_gtFine_color.png
 
     # 4. Loop on the batches of the dataset
     with torch.no_grad(): # NOT compute the gradient (we already computed in the previous step)
-        for batch_idx, (inputs, targets, file_names) in enumerate(val_loader):
+        for batch_idx, (inputs, targets, color_targets, file_names) in enumerate(val_loader):
             if batch_idx % 100 == 0: # Print every 100 batches
                 print(f"Batch {batch_idx}/{len(val_loader)}")
             inputs, targets = inputs.cuda(), targets.cuda()
@@ -59,6 +104,12 @@ def validate(epoch, new_model, val_loader, criterion, num_classes):
             total_intersections += inters
             total_unions += unions
 
+        #only enter the loop if we haven't saved both images    
+        if flag_save < 2:
+            save_images(flag_save, save_dir,inputs, file_names, preds, color_targets,file_name_1, file_name_2)
+
+
+    # print the two images i want to save
     # 5. Compute the metrics for the validation set 
     # 5.a Compute the accuracy metrics, i.e. mIoU and mean loss
     print("Computing the metrics for the training set...")
@@ -78,8 +129,11 @@ def validate(epoch, new_model, val_loader, criterion, num_classes):
     else:
         # NB: metric = -1 means we have not computed it (we compute only at the last epoch)
         mean_latency = -1
+        std_latency = -1
         num_flops = -1
         trainable_params = -1
+        mean_fps = -1
+        std_fps = -1
 
    
     # 6. Return all the metrics
@@ -88,6 +142,9 @@ def validate(epoch, new_model, val_loader, criterion, num_classes):
         'mean_iou': mean_iou,
         'iou_per_class': iou_per_class,
         'mean_latency' : mean_latency,
+        'std_latency' : std_latency,
+        'mean_fps' : mean_fps,
+        'std_fps' : std_fps,
         'num_flops' : num_flops,
         'trainable_params': trainable_params
     }
