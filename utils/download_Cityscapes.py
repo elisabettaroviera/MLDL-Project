9ace0e249f33da5d9be188ae6d731f5b19df3216
etta1509 <s328422@studenti.polito.it>
2025-05-13 22:39:32 +0200

    dataset

--- utils/download_Cityscapes.py
+++ utils/download_Cityscapes.py
@@ -10,9 +10,10 @@ cityscapes_url = f'https://drive.google.com/uc?id={cityscapes_id}' # Construct t
 
 
 #Personal Google Drive links for the datasets:
-cityscapes_id = '11JDxO0DX7MV5DVB225DuIW7vXP-tZGmb'  # Google Drive file ID
+cityscapes_id = '1HmmcWizKfXW8h7VzNzZ4c2cTkiMojR5J'  # Google Drive file ID
 # 10XPV2_VJ0kqSPhEHyleYwsalVdVSl1uI
 # 11JDxO0DX7MV5DVB225DuIW7vXP-tZGmb
+# 1HmmcWizKfXW8h7VzNzZ4c2cTkiMojR5J
 cityscapes_url = f'https://drive.google.com/uc?id={cityscapes_id}' # Construct the download URL
 
 
