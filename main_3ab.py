5c534358091c8e8ec1f57867ca6391d180756044
etta1509 <s328422@studenti.polito.it>
2025-05-14 16:30:41 +0200

    main3ab

--- main_3ab.py
+++ main_3ab.py
@@ -61,7 +61,7 @@ def print_metrics(title, metrics):
 
 
 if __name__ == "__main__":
-
+    var_model = os.environ['MODEL'] 
     set_seed(23)  # Set a seed for reproducibility
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
