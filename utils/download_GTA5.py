3a8da53415d0d95a7998c254cbb867994a1e841d
etta1509 <s328422@studenti.polito.it>
2025-05-17 19:58:10 +0200

    change dataset

--- utils/download_GTA5.py
+++ utils/download_GTA5.py
@@ -4,14 +4,14 @@ from zipfile import ZipFile
 from io import BytesIO
 import gdown
 
-gta5_id = '1xYxlcMR2WFCpayNrW2-Rb7N-950vvl23'  # Google Drive file ID
+gta5_id = '1W7lXYYeRl30jDvq01Kt9BlojFXKQglKO'  # Google Drive file ID
 gta5_url = f'https://drive.google.com/uc?id={gta5_id}' # Construct the download URL
 
 """
 
 Personal Google Drive links for the datasets:
 gta5_id = '1W7lXYYeRl30jDvq01Kt9BlojFXKQglKO'  # Google Drive file ID
-
+https://drive.google.com/file/d/1W7lXYYeRl30jDvq01Kt9BlojFXKQglKO/view?usp=sharing
 """
 
 gta5_output = 'gta5.zip' # Name of the output file
