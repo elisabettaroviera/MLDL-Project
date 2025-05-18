fb6ae9c37dd83ba559b91821d4ed138a197001cf
etta1509 <s328422@studenti.polito.it>
2025-05-18 14:26:00 +0200

    gtatocity ce07 + tv03 + epoch=1 geometric validation

--- main3ab_val.py
+++ main3ab_val.py
@@ -63,8 +63,13 @@ if __name__ == "__main__":
     cs_val = CityScapes('./datasets/Cityscapes', 'val', transform_cityscapes_dataset, target_transform_cityscapes)
 
     batch_size = 4
+    learning_rate = 0.00625
+    momentum = 0.9
+    weight_decay = 1e-4
+    num_epochs = 50
     num_classes = 19
     ignore_index = 255
+    start_epoch = 1
     loss = CombinedLoss_All(num_classes=num_classes, alpha=0.7, beta=0, gamma=0.3, theta=0, ignore_index=255)
 
     _, dataloader_cs_val = dataloader(None, cs_val, batch_size, shuffle_train=False, shuffle_val=False)
@@ -72,25 +77,23 @@ if __name__ == "__main__":
     model = BiSeNet(num_classes=num_classes, context_path='resnet18').to(device)
 
     project_name = "3b_GTA5_to_CITY_augmented_geometric_cv07_tv_03"
-    run = wandb.init(project=project_name, entity="s328422-politecnico-di-torino", name="BiSeNet_GTA5_Validation", reinit=True)
+    for epoch in range(start_epoch, num_epochs + 1):
+        run = wandb.init(project=project_name, entity="s328422-politecnico-di-torino", name=f"epoch_{epoch}", reinit=True)
+        artifact = wandb.use_artifact(f"{project_name}/model_epoch_{epoch}:latest", type="model")
+        artifact_path = artifact.download()
+        checkpoint_path = os.path.join(artifact_path, f"model_epoch_{epoch}.pt")
 
-    # Epoch to validate
-    epoch = 50  # PUT HERE THE EPOCH YOU WANT TO VALIDATE - THE EXACT NUMBER OF THE EPOCHE THAT YOU WANT TO VALIDATE
-    artifact = wandb.use_artifact(f"{project_name}/model_epoch_{epoch}:latest", type="model")
-    artifact_path = artifact.download()
-    checkpoint_path = os.path.join(artifact_path, f"model_epoch_{epoch}.pt")
+        checkpoint = torch.load(checkpoint_path)
+        model.load_state_dict(checkpoint['model_state_dict'])
 
-    checkpoint = torch.load(checkpoint_path)
-    model.load_state_dict(checkpoint['model_state_dict'])
+        print(f"Evaluating model from epoch {epoch}...")
+        start_val = time.time()
+        metrics_val = validate(epoch, model, dataloader_cs_val, loss, num_classes)
+        end_val = time.time()
+        print(f"Validation time: {(end_val - start_val)/60:.2f} min")
 
-    print(f"Evaluating model from epoch {epoch}...")
-    start_val = time.time()
-    metrics_val = validate(epoch, model, dataloader_cs_val, loss, num_classes)
-    end_val = time.time()
-    print(f"Validation time: {(end_val - start_val)/60:.2f} min")
-
-    print_metrics("Validation", metrics_val)
-    save_metrics_on_wandb(epoch, metrics_train=None, metrics_val=metrics_val)
-    save_metrics_on_file(epoch, metrics_train=None, metrics_val=metrics_val)
+        print_metrics("Validation", metrics_val)
+        save_metrics_on_wandb(epoch, metrics_train=None, metrics_val=metrics_val)
+        save_metrics_on_file(epoch, metrics_train=None, metrics_val=metrics_val)
 
     wandb.finish()
