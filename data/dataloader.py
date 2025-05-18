17c3a97dd90dbf1397630e93aeaa5b93b9080946
etta1509 <s328422@studenti.polito.it>
2025-05-18 11:14:37 +0200

    modification for empty dataloader_val

--- data/dataloader.py
+++ data/dataloader.py
@@ -6,5 +6,8 @@ from torch.utils.data import DataLoader
 # Create a DataLoader
 def dataloader(dataset_train, dataset_val, batch_size, shuffle_train=True, shuffle_val=True):
     dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=shuffle_train)
-    dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=shuffle_val)
+    if dataset_val is None:
+        return dataloader_train, None
+    else:
+        dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=shuffle_val)
     return dataloader_train, dataloader_val
