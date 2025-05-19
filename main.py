efc5ee7a662f6f655aca0dae7be4bd7224a3332a
Aurona Gashi <s281401@studenti.polito.it>
2025-05-19 19:55:01 +0200

    added print

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
     # -- DataLoader --
     full_dataloader_cs_train, dataloader_cs_val = dataloader(cs_train, cs_val, batch_size, True, True)
     dataloader_cs_train = select_random_fraction_of_dataset(full_dataloader_cs_train, fraction= 0.5, batch_size=batch_size)
-    print(f'the number of samples in the dataloader train is : len(dataloader_cs_train )')
+    print(f'the number of samples in the dataloader train is : {len(dataloader_cs_train )}')
 
 
 
