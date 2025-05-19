548069013c864c19e33f7ab2bf86823909d12db5
Aurona Gashi <s281401@studenti.polito.it>
2025-05-19 17:24:06 +0200

    back to previous version

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
     elif var_model == 'BiSeNet':
         model = BiSeNet(num_classes=num_classes, context_path='resnet18')
         # number of epoch that we want to start from
-        start_epoch = 1
+        start_epoch = 45
 
     # Load the model on the device    
     model = model.to(device)
@@ -164,7 +164,7 @@ if __name__ == "__main__":
         # Change the name of the project before the final run of 50 epochs
         # _cv07_di03
         # _cv07_tv03
-        wandb.init(project=f"{var_model}_prova_dice", entity="s328422-politecnico-di-torino", name=f"epoch_{epoch}", reinit=True) # Replace with your wandb entity name
+        wandb.init(project=f"{var_model}_cv07_di03", entity="s328422-politecnico-di-torino", name=f"epoch_{epoch}", reinit=True) # Replace with your wandb entity name
         print("Wandb initialized")
 
         print(f"Epoch {epoch}")
@@ -173,7 +173,7 @@ if __name__ == "__main__":
         # 1. Obtain the pretrained model
         if epoch != 1:
             # Load the model from the previous epoch using wandb artifact
-            artifact = wandb.use_artifact(f"s328422-politecnico-di-torino/{var_model}_prova_dice/model_epoch_{epoch-1}:latest", type="model")
+            artifact = wandb.use_artifact(f"s328422-politecnico-di-torino/{var_model}_cv07_di03/model_epoch_{epoch-1}:latest", type="model")
             
             # Get the local path where the artifact is saved
             artifact_dir = artifact.download()
