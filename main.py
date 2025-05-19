462d35505634a98c13d502f2bdc24189b4a89c3b
Aurona Gashi <s281401@studenti.polito.it>
2025-05-19 21:10:53 +0200

    start epoch 3

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ if __name__ == "__main__":
     elif var_model == 'BiSeNet':
         model = BiSeNet(num_classes=num_classes, context_path='resnet18')
         # number of epoch that we want to start from
-        start_epoch = 2
+        start_epoch = 3
 
     # Load the model on the device    
     model = model.to(device)
