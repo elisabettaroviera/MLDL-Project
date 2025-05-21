d58ec29d397d108c2dac7ece5e877b0a6ed97db4
etta1509 <s328422@studenti.polito.it>
2025-05-21 18:25:32 +0200

    drop of the last batch

--- main3ab_train.py
+++ main3ab_train.py
@@ -66,7 +66,7 @@ def select_random_fraction_of_dataset(full_dataloader, fraction=1.0, batch_size=
 
     # Crea un subset e un nuovo dataloader
     subset = Subset(dataset, indices)
-    subset_dataloader, _ = dataloader(subset, None, batch_size, True, True)
+    subset_dataloader, _ = dataloader(subset, None, batch_size, True, True, True) # Drop of the last batch
 
     return subset_dataloader
 
