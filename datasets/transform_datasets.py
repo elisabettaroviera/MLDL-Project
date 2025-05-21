0133fdc06aece0597e5c0c70237cadc19c76738b
LuciaGhezzi <s325951@studenti.polito.it>
2025-05-21 16:37:34 +0200

    name transformations

--- datasets/transform_datasets.py
+++ datasets/transform_datasets.py
@@ -175,27 +175,27 @@ def augmentation_transform(image, mask, type_aug_dict):
 
     # --- COLOR ---
     color_transforms = {
-        'HueSaturationValue': A.HueSaturationValue(hue_shift_limit=10, sat_shift_limit=15, val_shift_limit=10, p=1.0),
-        'CLAHE': A.CLAHE(clip_limit=2.0, tile_grid_size=(8, 8), p=1.0),
-        'GaussNoise': A.GaussNoise(var_limit=(10.0, 50.0), mean=0, p=1.0),
-        'RGBShift': A.RGBShift(r_shift_limit=10, g_shift_limit=10, b_shift_limit=10, p=1.0),
-        'RandomBrightnessContrast': A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1.0)
+        'HueSaturationValue': A.HueSaturationValue(hue_shift_limit=10, sat_shift_limit=15, val_shift_limit=10, p=1.0), #a)
+        'CLAHE': A.CLAHE(clip_limit=2.0, tile_grid_size=(8, 8), p=1.0), #b)
+        'GaussNoise': A.GaussNoise(var_limit=(10.0, 50.0), mean=0, p=1.0), #c)
+        'RGBShift': A.RGBShift(r_shift_limit=10, g_shift_limit=10, b_shift_limit=10, p=1.0), #d)
+        'RandomBrightnessContrast': A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1.0) #e)
     }
 
     # --- WEATHER ---
     weather_transforms = {
-        'RandomShadow': A.RandomShadow(shadow_roi=(0, 0.5, 1, 1), num_shadows_lower=1, num_shadows_upper=2, p=1.0),
-        'RandomFog': A.RandomFog(fog_coef_lower=0.05, fog_coef_upper=0.15, alpha_coef=0.1, p=1.0),
-        'RandomRain': A.RandomRain(blur_value=2, drop_length=10, drop_width=1, brightness_coefficient=0.95, p=1.0),
-        'ISONoise': A.ISONoise(color_shift=(0.01, 0.05), intensity=(0.1, 0.3), p=1.0),
-        'GaussianBlur': A.GaussianBlur(blur_limit=(3, 5), sigma_limit=0.5, p=1.0)
+        'RandomShadow': A.RandomShadow(shadow_roi=(0, 0.5, 1, 1), num_shadows_lower=1, num_shadows_upper=2, p=1.0), #f)
+        'RandomFog': A.RandomFog(fog_coef_lower=0.05, fog_coef_upper=0.15, alpha_coef=0.1, p=1.0), #g)
+        'RandomRain': A.RandomRain(blur_value=2, drop_length=10, drop_width=1, brightness_coefficient=0.95, p=1.0), #h)
+        'ISONoise': A.ISONoise(color_shift=(0.01, 0.05), intensity=(0.1, 0.3), p=1.0), #i)
+        'GaussianBlur': A.GaussianBlur(blur_limit=(3, 5), sigma_limit=0.5, p=1.0) #l)
     }
 
     # --- GEOMETRIC ---
     geometric_transforms = {
-        'RandomCrop': A.RandomCrop(height=720, width=1280, p=1.0),
-        'Affine': A.Affine(scale=(0.95, 1.05), translate_percent=(0.02, 0.05), rotate=(-5, 5), shear=(-2, 2), p=1.0),
-        'Perspective': A.Perspective(scale=(0.02, 0.05), keep_size=True, p=1.0)
+        'RandomCrop': A.RandomCrop(height=720, width=1280, p=1.0), #m)
+        'Affine': A.Affine(scale=(0.95, 1.05), translate_percent=(0.02, 0.05), rotate=(-5, 5), shear=(-2, 2), p=1.0), #n)
+        'Perspective': A.Perspective(scale=(0.02, 0.05), keep_size=True, p=1.0) #o)
     }
 
     if 'color' in type_aug_dict:
