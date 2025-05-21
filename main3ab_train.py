3d1e43c37014e5459a015934be40beac87a7f8e4
Aurona Gashi <s281401@studenti.polito.it>
2025-05-21 21:10:05 +0200

    start epoch 1 weather f

--- main3ab_train.py
+++ main3ab_train.py
@@ -107,8 +107,8 @@ if __name__ == "__main__":
     }
 """
 
-    #type_aug = {'weather': ['RandomShadow']} #sta runnando in aurona 2 holaatutti
-    type_aug = {'weather': ['RandomFog']} #sta runnando in aurona 3 auronagashi23
+    type_aug = {'weather': ['RandomShadow']} #sta runnando in aurona 2 holaatutti
+    #type_aug = {'weather': ['RandomFog']} #sta runnando in aurona 3 auronagashi23
     #type_aug = {'weather': ['RandomRain']} #sta runnando in aurona 4 mail familje
     gta_train_nonaug = GTA5('./datasets/GTA5', transform_gta_dataset, target_transform_gta, augmentation=False, type_aug={}) # No type_aug 
     # Contains all pictures bc they are all augmented
@@ -143,7 +143,7 @@ if __name__ == "__main__":
     iter_curr = 0
 
     for epoch in range(start_epoch, num_epochs + 1):
-        project_name = "3b_GTA5_to_CITY_augmented_weather_g_25_percentage" #CHECK BEFORE RUNNING
+        project_name = "3b_GTA5_to_CITY_augmented_weather_f_25_percentage" #CHECK BEFORE RUNNING
         entity = "s325951-politecnico-di-torino-mldl" # new team Lucia
         # entity="s328422-politecnico-di-torino" # old team Betta
         run = wandb.init(project=project_name, entity=entity, name=f"epoch_{epoch}", reinit=True)
