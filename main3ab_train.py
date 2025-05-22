28188e09f0157a74d7fcea40e0ad294238424e75
LuciaGhezzi <s325951@studenti.polito.it>
2025-05-22 13:22:55 +0200

    train b+e

--- main3ab_train.py
+++ main3ab_train.py
@@ -116,8 +116,8 @@ if __name__ == "__main__":
 
     # b + cose
     #type_aug = { 'color': ['CLAHE', 'GaussNoise']} # b+c) 3b_GTA5_to_CITY_aug_color_b_c_25percent
-    type_aug = { 'color': ['CLAHE', 'RGBShift']} # b+d) 3b_GTA5_to_CITY_aug_color_b_d_25percent
-    #type_aug = { 'color': ['CLAHE', 'RandomBrightnessContrast']} # b+e) 3b_GTA5_to_CITY_aug_color_b_e_25percent
+    #type_aug = { 'color': ['CLAHE', 'RGBShift']} # b+d) 3b_GTA5_to_CITY_aug_color_b_d_25percent
+    type_aug = { 'color': ['CLAHE', 'RandomBrightnessContrast']} # b+e) 3b_GTA5_to_CITY_aug_color_b_e_25percent
 
     # c + cose
     #type_aug = { 'color': ['GaussNoise', 'RGBShift']} # c+d) 3b_GTA5_to_CITY_aug_color_c_d_25percent
@@ -160,7 +160,7 @@ if __name__ == "__main__":
     iter_curr = 0
 
     for epoch in range(start_epoch, num_epochs + 1):
-        project_name = "3b_GTA5_to_CITY_aug_color_b_d_25percent" #CHECK BEFORE RUNNING________________________________________HERE
+        project_name = "3b_GTA5_to_CITY_aug_color_b_e_25percent" #CHECK BEFORE RUNNING________________________________________HERE
         entity = "s325951-politecnico-di-torino-mldl" # new team Lucia
         # entity="s328422-politecnico-di-torino" # old team Betta
         run = wandb.init(project=project_name, entity=entity, name=f"epoch_{epoch}", reinit=True)
