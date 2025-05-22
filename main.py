64b1bc6041986ec1794b159a4521a2a67e209655
etta1509 <s328422@studenti.polito.it>
2025-05-22 21:16:56 +0200

    import all from utils.utils

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import torchvision.transforms.functional as TF
 from datasets.cityscapes import CityScapes
 import random
 from train import train
-from utils.utils import CombinedLoss_All, poly_lr_scheduler, save_metrics_on_file, save_metrics_on_wandb
+from utils.utils import *
 from validation import validate
 from utils.metrics import compute_miou
 from torch.utils.data import Subset
