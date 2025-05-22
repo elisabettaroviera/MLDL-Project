b3dca17c5653c88dd041629b145f8ca301297ce3
Aurona Gashi <s281401@studenti.polito.it>
2025-05-22 20:30:30 +0200

    -

--- utils/download_GTA5.py
+++ utils/download_GTA5.py
@@ -8,11 +8,11 @@ import gdown
 # Link 1 - 1xYxlcMR2WFCpayNrW2-Rb7N-950vvl23
 # Link 2 - 1U5LpT_jGqTp2wuDvGHewTOnjbCCjVWE2
 # Link 3 - 1eL0EhtP_TYs8QY0WBRkfC665jYiBW3Fl
-# Link 4 - 1291JV6dHH0YkT0jI2lgRZXtAXSWB5m90
+# Link 4 - 1291JV6dHH0YkT0jI2lgRZXtAXSWB5m90 
 # Link 5 - 1T4sLX0HLpI6kqe4V2F2oVAzur5fsp4Uc
 # Link 6 - 1dJ8HS9Z6XQtDwI1RBDQm89orz69npVTM
 # Link 7 - 1W7lXYYeRl30jDvq01Kt9BlojFXKQglKO
-gta_id ='1dJ8HS9Z6XQtDwI1RBDQm89orz69npVTM'
+gta5_id ='1dJ8HS9Z6XQtDwI1RBDQm89orz69npVTM'
 gta5_url = f'https://drive.google.com/uc?id={gta5_id}' # Construct the download URL
 
 """
