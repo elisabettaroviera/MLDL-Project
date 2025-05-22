753857887db3f1399ddd40d8d3f5db4e82862101
Aurona Gashi <s281401@studenti.polito.it>
2025-05-22 16:09:49 +0200

    set i for aurona 3

--- main3ab_train.py
+++ main3ab_train.py
@@ -107,9 +107,10 @@ if __name__ == "__main__":
     }
 """
 
-    type_aug = {'weather': ['RandomShadow']} #sta runnando in aurona 2 holaatutti
-    #type_aug = {'weather': ['RandomFog']} #sta runnando in aurona 3 auronagashi23
-    type_aug = {'weather': ['RandomRain']} #sta runnando in aurona 4 mail familje
+    type_aug = {'weather': ['RandomShadow']} #sta runnando in aurona 2 holaatutti f
+    #type_aug = {'weather': ['RandomFog']} #FINITO ANCEH VAL in aurona 3 auronagashi23 g
+    type_aug = {'weather': ['RandomRain']} #sta runnando in aurona 4 mail familje h
+    type_aug = {'weather': ['ISONoise']} #runnando in aurona3 auronagashi23 i
     gta_train_nonaug = GTA5('./datasets/GTA5', transform_gta_dataset, target_transform_gta, augmentation=False, type_aug={}) # No type_aug 
     # Contains all pictures bc they are all augmented
     gta_train_aug = GTA5('./datasets/GTA5', transform_gta_dataset, target_transform_gta, augmentation=True, type_aug=type_aug) # Change the augm that you want
@@ -143,7 +144,7 @@ if __name__ == "__main__":
     iter_curr = 0
 
     for epoch in range(start_epoch, num_epochs + 1):
-        project_name = "3b_GTA5_to_CITY_augmented_weather_h_25_percentage" #CHECK BEFORE RUNNING
+        project_name = "3b_GTA5_to_CITY_augmented_weather_i_25_percentage" #CHECK BEFORE RUNNING
         entity = "s325951-politecnico-di-torino-mldl" # new team Lucia
         # entity="s328422-politecnico-di-torino" # old team Betta
         run = wandb.init(project=project_name, entity=entity, name=f"epoch_{epoch}", reinit=True)
