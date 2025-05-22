81df02b941c37a366c8bd0d4ebe1c57c7881a66b
LuciaGhezzi <s325951@studenti.polito.it>
2025-05-22 14:05:21 +0200

    train c+d

--- main3ab_train.py
+++ main3ab_train.py
@@ -110,17 +110,17 @@ if __name__ == "__main__":
     ## 2 TRASFORMAZIONI : 
     # a + cose
     #type_aug = { 'color': ['HueSaturationValue', 'CLAHE']} # a+b) 3b_GTA5_to_CITY_aug_color_a_b_25percent OK
-    #type_aug = { 'color': ['HueSaturationValue', 'GaussNoise']} # a+c) 3b_GTA5_to_CITY_aug_color_a_c_25percent 
-    #type_aug = { 'color': ['HueSaturationValue', 'RGBShift']} # a+d) 3b_GTA5_to_CITY_aug_color_a_d_25percent
-    #type_aug = { 'color': ['HueSaturationValue', 'RandomBrightnessContrast']} # a+e) 3b_GTA5_to_CITY_aug_color_a_e_25percent
+    #type_aug = { 'color': ['HueSaturationValue', 'GaussNoise']} # a+c) 3b_GTA5_to_CITY_aug_color_a_c_25percent OK
+    #type_aug = { 'color': ['HueSaturationValue', 'RGBShift']} # a+d) 3b_GTA5_to_CITY_aug_color_a_d_25percent OK
+    #type_aug = { 'color': ['HueSaturationValue', 'RandomBrightnessContrast']} # a+e) 3b_GTA5_to_CITY_aug_color_a_e_25percent OK
 
     # b + cose
-    #type_aug = { 'color': ['CLAHE', 'GaussNoise']} # b+c) 3b_GTA5_to_CITY_aug_color_b_c_25percent
-    #type_aug = { 'color': ['CLAHE', 'RGBShift']} # b+d) 3b_GTA5_to_CITY_aug_color_b_d_25percent
-    type_aug = { 'color': ['CLAHE', 'RandomBrightnessContrast']} # b+e) 3b_GTA5_to_CITY_aug_color_b_e_25percent
+    #type_aug = { 'color': ['CLAHE', 'GaussNoise']} # b+c) 3b_GTA5_to_CITY_aug_color_b_c_25percent OK
+    #type_aug = { 'color': ['CLAHE', 'RGBShift']} # b+d) 3b_GTA5_to_CITY_aug_color_b_d_25percent OK
+    #type_aug = { 'color': ['CLAHE', 'RandomBrightnessContrast']} # b+e) 3b_GTA5_to_CITY_aug_color_b_e_25percent OK
 
     # c + cose
-    #type_aug = { 'color': ['GaussNoise', 'RGBShift']} # c+d) 3b_GTA5_to_CITY_aug_color_c_d_25percent
+    type_aug = { 'color': ['GaussNoise', 'RGBShift']} # c+d) 3b_GTA5_to_CITY_aug_color_c_d_25percent
     #type_aug = { 'color': ['GaussNoise', 'RandomBrightnessContrast']} # c+e) 3b_GTA5_to_CITY_aug_color_c_e_25percent
 
     # d + cose
@@ -160,7 +160,7 @@ if __name__ == "__main__":
     iter_curr = 0
 
     for epoch in range(start_epoch, num_epochs + 1):
-        project_name = "3b_GTA5_to_CITY_aug_color_b_e_25percent" #CHECK BEFORE RUNNING________________________________________HERE
+        project_name = "3b_GTA5_to_CITY_aug_color_c_d_25percent" #CHECK BEFORE RUNNING________________________________________HERE
         entity = "s325951-politecnico-di-torino-mldl" # new team Lucia
         # entity="s328422-politecnico-di-torino" # old team Betta
         run = wandb.init(project=project_name, entity=entity, name=f"epoch_{epoch}", reinit=True)
