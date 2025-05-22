8fb95a2ceb64c3d9fb4f669872feb9317ad72bb8
etta1509 <s328422@studenti.polito.it>
2025-05-22 17:21:05 +0200

    Reordering links

--- utils/download_Cityscapes.py
+++ utils/download_Cityscapes.py
@@ -6,10 +6,10 @@ import gdown
 
 # Personal Google Drive links for the datasets - CHOOSE one of them
 # Link 1 - 1HmmcWizKfXW8h7VzNzZ4c2cTkiMojR5J
-# Link 2 - 1Qb4UrNsjvlU-wEsR9d7rckB0YS_LXgb2
+# Link 2 - 1qKP1oqImbrMfDI9R9dJpnlLYn_hgS8Ht
 # Link 3 - 10XPV2_VJ0kqSPhEHyleYwsalVdVSl1uI
 # Link 4 - 11JDxO0DX7MV5DVB225DuIW7vXP-tZGmb
-# Link 5 - 1qKP1oqImbrMfDI9R9dJpnlLYn_hgS8Ht
+# Link 5 - 1Qb4UrNsjvlU-wEsR9d7rckB0YS_LXgb2
 
 cityscapes_id = '1HmmcWizKfXW8h7VzNzZ4c2cTkiMojR5J'  # Google Drive file ID
 cityscapes_url = f'https://drive.google.com/uc?id={cityscapes_id}' # Construct the download URL
