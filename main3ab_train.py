26ce518a00b4ed7d8368c2b55e1a5a5475868231
LuciaGhezzi <s325951@studenti.polito.it>
2025-05-22 09:00:53 +0200

    id gta5

--- main3ab_train.py
+++ main3ab_train.py
@@ -103,8 +103,8 @@ if __name__ == "__main__":
 
     #type_aug = { 'color': ['HueSaturationValue']} #a) 3b_GTA5_to_CITY_aug_color_a_25percent
     #type_aug = {'color': ['CLAHE']} # b) 3b_GTA5_to_CITY_aug_color_b_25percent
-    type_aug = {'color': ['GaussNoise']} # c) 3b_GTA5_to_CITY_aug_color_c_25percent
-    #type_aug = {'color': ['RGBShift']} # d) 3b_GTA5_to_CITY_aug_color_d_25percent
+    #type_aug = {'color': ['GaussNoise']} # c) 3b_GTA5_to_CITY_aug_color_c_25percent
+    type_aug = {'color': ['RGBShift']} # d) 3b_GTA5_to_CITY_aug_color_d_25percent
     #type_aug = {'color': ['RandomBrightnessContrast']} # e) 3b_GTA5_to_CITY_aug_color_e_25percent
     
     gta_train_nonaug = GTA5('./datasets/GTA5', transform_gta_dataset, target_transform_gta, augmentation=False, type_aug={}) # No type_aug 
@@ -140,7 +140,7 @@ if __name__ == "__main__":
     iter_curr = 0
 
     for epoch in range(start_epoch, num_epochs + 1):
-        project_name = "3b_GTA5_to_CITY_aug_color_c_25percent" #CHECK BEFORE RUNNING
+        project_name = "3b_GTA5_to_CITY_aug_color_d_25percent" #CHECK BEFORE RUNNING
         entity = "s325951-politecnico-di-torino-mldl" # new team Lucia
         # entity="s328422-politecnico-di-torino" # old team Betta
         run = wandb.init(project=project_name, entity=entity, name=f"epoch_{epoch}", reinit=True)
