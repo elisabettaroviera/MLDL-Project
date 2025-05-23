b27b10055e430bba1f806c2e165f9ea868d148a0
etta1509 <s328422@studenti.polito.it>
2025-05-23 08:23:27 +0200

    new link

--- utils/download_Cityscapes.py
+++ utils/download_Cityscapes.py
@@ -11,7 +11,7 @@ import gdown
 # Link 5 - 1Qb4UrNsjvlU-wEsR9d7rckB0YS_LXgb2
 # Link 6 - 11radYwi905384YHAmC_AGvziOcqBpE4X
 
-cityscapes_id = '1Qb4UrNsjvlU-wEsR9d7rckB0YS_LXgb2'  # Google Drive file ID
+cityscapes_id = '11radYwi905384YHAmC_AGvziOcqBpE4X'  # Google Drive file ID
 cityscapes_url = f'https://drive.google.com/uc?id={cityscapes_id}' # Construct the download URL
 cityscapes_output = 'cityscapes.zip' # Name of the output file
 
