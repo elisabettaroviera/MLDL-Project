f082e526851f4d66152a7de254ead6d1fce9e497
LuciaGhezzi <s325951@studenti.polito.it>
2025-05-23 10:33:46 +0200

    train a+b+d

--- main3ab_train.py
+++ main3ab_train.py
@@ -121,15 +121,15 @@ if __name__ == "__main__":
 
     # c + cose
     #type_aug = { 'color': ['GaussNoise', 'RGBShift']} # c+d) 3b_GTA5_to_CITY_aug_color_c_d_25percent OKK
-    #type_aug = { 'color': ['GaussNoise', 'RandomBrightnessContrast']} # c+e) 3b_GTA5_to_CITY_aug_color_c_e_25percent OK
+    #type_aug = { 'color': ['GaussNoise', 'RandomBrightnessContrast']} # c+e) 3b_GTA5_to_CITY_aug_color_c_e_25percent OKK
 
     # d + cose
-    #type_aug = { 'color': ['RGBShift', 'RandomBrightnessContrast']} # d+e) 3b_GTA5_to_CITY_aug_color_d_e_25percent OK
+    #type_aug = { 'color': ['RGBShift', 'RandomBrightnessContrast']} # d+e) 3b_GTA5_to_CITY_aug_color_d_e_25percent OKK
 
     ## 3 TRASFORMAZIONI
     # a + b + cose
-    type_aug = { 'color': ['HueSaturationValue', 'CLAHE', 'GaussNoise']} # a+b+c) 3b_GTA5_to_CITY_aug_color_a_b_c_25percent
-    #type_aug = { 'color': ['HueSaturationValue', 'CLAHE', 'RGBShift']} # a+b) 3b_GTA5_to_CITY_aug_color_a_b_d_25percent 
+    #type_aug = { 'color': ['HueSaturationValue', 'CLAHE', 'GaussNoise']} # a+b+c) 3b_GTA5_to_CITY_aug_color_a_b_c_25percent OK
+    type_aug = { 'color': ['HueSaturationValue', 'CLAHE', 'RGBShift']} # a+b) 3b_GTA5_to_CITY_aug_color_a_b_d_25percent  
     #type_aug = { 'color': ['HueSaturationValue', 'CLAHE', 'RandomBrightnessContrast']} # a+b) 3b_GTA5_to_CITY_aug_color_a_b_e_25percent
     # a + c + cose
     #type_aug = { 'color': ['HueSaturationValue', 'GaussNoise', 'RGBShift']} # a+c) 3b_GTA5_to_CITY_aug_color_a_c_d_25percent
@@ -172,7 +172,7 @@ if __name__ == "__main__":
     iter_curr = 0
 
     for epoch in range(start_epoch, num_epochs + 1):
-        project_name = "3b_GTA5_to_CITY_aug_color_a_b_c_25percent" #CHECK BEFORE RUNNING________________________________________HERE
+        project_name = "3b_GTA5_to_CITY_aug_color_a_b_d_25percent" #CHECK BEFORE RUNNING________________________________________HERE
         entity = "s325951-politecnico-di-torino-mldl" # new team Lucia
         # entity="s328422-politecnico-di-torino" # old team Betta
         run = wandb.init(project=project_name, entity=entity, name=f"epoch_{epoch}", reinit=True)
