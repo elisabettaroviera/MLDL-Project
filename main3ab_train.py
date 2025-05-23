22dbeec76fd1982905029973d77a2f87e914107d
Aurona Gashi <s281401@studenti.polito.it>
2025-05-23 16:12:14 +0200

    sem12

--- main3ab_train.py
+++ main3ab_train.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     num_epochs = 15 #changed bc doing smaller runs
     num_classes = 19
     ignore_index = 255
-    start_epoch = 1 #CHECK BEFORE RUNNING
+    start_epoch = 12 #CHECK BEFORE RUNNING
 
     # Transformation
     transform_gta_dataset = transform_gta()
