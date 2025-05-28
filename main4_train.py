f5c947f7e885f26e089527c7201d5e7c1f1ba8a7
Giacomo Maino <mainogiacomo18@gmail.com>
2025-05-28 12:13:37 +0200

    Updated folder name in main4_train to work

--- main4_train.py
+++ main4_train.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
     
     # Create dataloader
     full_dataloader_gta_train, _ = dataloader(gta_train, None, batch_size, True, True)
-    full_dataloader_cityscapes_train, _ = dataloader(CityScapes('./datasets/CityScapes'), None, batch_size, True, True)
+    full_dataloader_cityscapes_train, _ = dataloader(CityScapes('./datasets/Cityscapes'), None, batch_size, True, True)
     # Take a subset of the dataloader
     dataloader_gta_train = select_random_fraction_of_dataset(full_dataloader_gta_train, fraction=0.25, batch_size=batch_size)
     
