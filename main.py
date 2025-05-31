9798ca757ae43aaad2a5b280a801fa24f0d46a36
etta1509 <s328422@studenti.polito.it>
2025-05-31 16:00:56 +0200

    Ce01 + warmup 1110 + new modifiche + lr=0.00625

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     elif var_model == 'BiSeNet':
         print("MODEL BISENET")
         batch_size = 4 # Bach size
-        learning_rate = 0.005 # Learning rate for the optimizer - 1e-4
+        learning_rate = 0.00625 # Learning rate for the optimizer - 1e-4
         momentum = 0.9 # Momentum for the optimizer
         weight_decay = 1e-4 # Weight decay for the optimizer
 
@@ -156,7 +156,7 @@ if __name__ == "__main__":
         # baseline_lr_0.00625_ce1_warmup1100_alpha1
         # _lr_0.005_ce1_warmup1100_alpha1
         
-        project_name = f"{var_model}_lr_0.005_ce1_warmup1100_alpha1" # CHANGE HERE THE PROJECT NAME
+        project_name = f"{var_model}_baseline_lr_0.00625_ce1_warmup1100_alpha1" # CHANGE HERE THE PROJECT NAME
         wandb.init(project=project_name, entity=entity, name=f"epoch_{epoch}", reinit=True) 
         print("Wandb initialized")
 
