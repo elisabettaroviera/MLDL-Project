74940fec21b2742eeb80e1a8263e13af17a9c1bf
etta1509 <s328422@studenti.polito.it>
2025-06-02 15:00:06 +0200

    Ce005 + F05 + warmup 2500 + lr=0.0005 starting_epoch = 43 WEIGHTED focal_gamma=4

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
         print("Load the model")
         model = get_deeplab_v2(num_classes=num_classes, pretrain=True, pretrain_model_path=pretrain_model_path)
        
-        start_epoch = 31 # CHANGE HERE THE STARTING EPOCH
+        start_epoch = 43 # CHANGE HERE THE STARTING EPOCH
 
 
     elif var_model == 'BiSeNet':
@@ -178,7 +178,7 @@ if __name__ == "__main__":
 
             # Defintion of the loss function CombinedLoss_All
             print("Definition of the loss") 
-            loss = CombinedLoss_All(num_classes=num_classes, alpha=0.5, beta=0, gamma=0, theta=0, delta=0.5, focal_gamma=3, ignore_index=255, class_weights=class_weights) # CHANGE HERE THE LOSS
+            loss = CombinedLoss_All(num_classes=num_classes, alpha=0.5, beta=0, gamma=0, theta=0, delta=0.5, focal_gamma=4, ignore_index=255, class_weights=class_weights) # CHANGE HERE THE LOSS
             # alpha   - CrossEntropy
         # beta    - Lovász
         # gamma   - Tversky
