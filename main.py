912b6149b02ec7c2da81a0402b5fd84054d55ee3
etta1509 <s328422@studenti.polito.it>
2025-06-02 11:28:56 +0200

    Ce05 + H05 + warmup 3000 + lr=0.00625 starting_epoch = 1 WEIGHTED + drop out + all the previous change + ALPHA_1=0.75 + Adam

--- main.py
+++ main.py
@@ -157,8 +157,10 @@ if __name__ == "__main__":
         # _lr_0.005_ce1_warmup1100_alpha1
         # _lr_0.00625_ce1_warmup1100_alpha0.4
         # _lr_0.00625_ce1_warmup2500_alpha1_weighted
+        # _lr_0.00625_ce07_tvf03_warmup3000_weighted_AdamW
+        # _lr_0.00625_ce05_h05_warmup3000_weighted_AdamW
         
-        project_name = f"{var_model}_lr_0.00625_ce07_tvf03_warmup3000_weighted_AdamW" # CHANGE HERE THE PROJECT NAME
+        project_name = f"{var_model}_lr_0.00625_ce05_h05_warmup3000_weighted_AdamW" # CHANGE HERE THE PROJECT NAME
         wandb.init(project=project_name, entity=entity, name=f"epoch_{epoch}", reinit=True) 
         print("Wandb initialized")
 
@@ -189,7 +191,7 @@ if __name__ == "__main__":
             
             # Defintion of the loss function CombinedLoss_All
             print("Definition of the loss") 
-            loss = CombinedLoss_All(num_classes=num_classes, alpha=0.7, beta=0, gamma=0.3, theta=0, delta=0, epsilon=0, focal_gamma=2, ignore_index=255, class_weights=class_weights) # CHANGE HERE THE LOSS
+            loss = CombinedLoss_All(num_classes=num_classes, alpha=0.5, beta=0, gamma=0.3, theta=0, delta=0, epsilon=0.5, focal_gamma=2, ignore_index=255, class_weights=class_weights) # CHANGE HERE THE LOSS
             # alpha   - CrossEntropy
             # beta    - Lovász
             # gamma   - Tversky
@@ -199,7 +201,7 @@ if __name__ == "__main__":
         elif epoch == 1:
             # Definition of the loss function CombinedLoss_All
             print("Definition of the loss") 
-            loss = CombinedLoss_All(num_classes=num_classes, alpha=0.7, beta=0, gamma=0.3, theta=0, delta=0, epsilon=0, focal_gamma=2, ignore_index=255)
+            loss = CombinedLoss_All(num_classes=num_classes, alpha=0.5, beta=0, gamma=0.3, theta=0, delta=0, epsilon=0.5, focal_gamma=2, ignore_index=255)
             
         print(f"Epoch {epoch}")
 
