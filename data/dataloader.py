80bc4e633feaec80e160d7638cd128b877a2e189
LuciaGhezzi <s325951@studenti.polito.it>
2025-06-04 14:01:44 +0200

    copied stuff 4 from branch Betta

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,14 +1,18 @@
 from torch.utils.data import DataLoader
 
 # Create a DataLoader
-def dataloader(dataset_train, dataset_val, batch_size, shuffle_train=True, shuffle_val=True, drop_last_bach=False):
-    if dataset_val is None:
-        dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=shuffle_train, drop_last=drop_last_bach)
+def dataloader(dataset_train, dataset_val, batch_size, shuffle_train=True, shuffle_val=True, drop_last_bach=False, num_workers=2):
+    # drop_last_bach = Ture: if the last bach has NOT batch_size element, just remove it
+
+    if dataset_val is None: # Only to train
+        dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=shuffle_train, drop_last=drop_last_bach, num_workers=num_workers, pin_memory=True)
         return dataloader_train, None
-    elif dataset_train is None:
-        dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=shuffle_val, drop_last=drop_last_bach)
+    
+    elif dataset_train is None: # Only to validate
+        dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=shuffle_val, drop_last=drop_last_bach, num_workers=num_workers, pin_memory=True)
         return None, dataloader_val
-    else:
-        dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=shuffle_train, drop_last=drop_last_bach)
-        dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=shuffle_val, drop_last=drop_last_bach)
+    
+    else: # To train and validate at the same time
+        dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=shuffle_train, drop_last=drop_last_bach, num_workers=num_workers, pin_memory=True)
+        dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=shuffle_val, drop_last=drop_last_bach, num_workers=num_workers, pin_memory=True)
         return dataloader_train, dataloader_val
--- train.py
+++ train.py
@@ -1,6 +1,3 @@
-# TODO: Define here your training and validation loops.
-
-# Import necessary libraries
 import os
 import torch
 from torchvision.datasets import ImageFolder
@@ -15,16 +12,56 @@ from datasets.cityscapes import CityScapes
 from utils.metrics import compute_miou, compute_latency_and_fps, compute_flops, compute_parameters
 from utils.utils import poly_lr_scheduler
 import wandb
+import gc
+
+bce_loss = torch.nn.BCEWithLogitsLoss(reduction="mean")
+softmax = torch.nn.functional.softmax
+
+def lock_model(model):
+    """
+    Lock the model parameters to avoid training them.
+    """
+    for param in model.parameters():
+        param.requires_grad = False
+    return model
+
+def unlock_model(model):
+    """
+    Unlock the model parameters to allow training.
+    """
+    for param in model.parameters():
+        param.requires_grad = True
+    return model
+
+def backpropagate(optimizer, loss):
+    """
+    Perform backpropagation and optimization step.
+    """
+    optimizer.zero_grad()  # Zero the gradients
+    loss.backward()        # Backpropagate the loss
+    optimizer.step()       # Update the model parameters
+    return optimizer
+
+def adversarial_loss(discriminators, outputs, target_label, source_label, device, lambdas):
+    total_adv_loss = 0.0
+    for i, discriminator in enumerate(discriminators):
+        disc_pred = discriminator(softmax(outputs[0], dim=1).detach())
+        target_tensor = torch.full(disc_pred.shape, float(source_label), device=device, dtype=disc_pred.dtype)
+        adv_loss = bce_loss(disc_pred, target_tensor)
+        total_adv_loss += lambdas[i]*adv_loss
+    return total_adv_loss
 
 # TRAIN LOOP
-def train(epoch, old_model, dataloader_train, criterion, optimizer, iter, learning_rate, num_classes, max_iter): # criterion = loss
+def train(epoch, old_model, dataloader_train, criterion, optimizer, iteration, learning_rate, num_classes, max_iter): # criterion == loss function
     var_model = os.environ['MODEL'] 
+
     # 1. Obtain the pretrained model
     model = old_model 
     print("Training the model...")
 
-    # 2. Initialize the metrics variables and hyperparameters(?)
+    # 2. Initialize the metrics variables and hyperparameters
     print("Initializing the metrics variables...")
+    
     running_loss = 0.0 
     mean_loss = 0.0
     total_intersections = np.zeros(num_classes)
@@ -36,26 +73,12 @@ def train(epoch, old_model, dataloader_train, criterion, optimizer, iter, learni
 
     print(f"Training on {len(dataloader_train)} batches")
     
-    # IMPORTANT TO DISCUSS:
-    # Problem to fix: decay of learning rate
-    # In the paper we can see that the learning rate decays with each batch.
-    # Starting from 0.001 and updating for all 20K iterations (number of batches * epochs).
-    # We can use the poly_lr_scheduler function to update the learning rate.
-    # But this way we have max_iter = 39K (number of batches * epochs) (39300 = 50 * 786)
-    # So I think we can try to update the learning rate one iteration yes and one iteration no on the batches.
-
-    # SOLUTION:
-    # We decide to use the poly_lr_scheduler function to update the learning rate
-    # The initial learning rate is _0.0001_ and the maximum number of iterations is 39K (number of batches * epochs).
-    # The learning rate is updated every iterations (batches)
-
-
     # 4. Loop on the batches of the dataset
-    for batch_idx, (inputs, targets, file_names) in enumerate(dataloader_train): #(X,y)
+    for batch_idx, (inputs, targets, file_names) in enumerate(dataloader_train): 
         if batch_idx % 100 == 0: # Print every 100 batches
             print(f"Batch {batch_idx}/{len(dataloader_train)}")
 
-        iter += 1 # Increment the iteration counter
+        iteration += 1 # Increment the iteration counter
 
         inputs, targets = inputs.cuda(), targets.cuda() # GPU
 
@@ -67,7 +90,7 @@ def train(epoch, old_model, dataloader_train, criterion, optimizer, iter, learni
         # BiseNet returns the output, aux1, aux2 (aux are predictions from contextpath)
         loss = criterion(outputs[0], targets)
         if var_model == "BiSeNet":
-            alpha = 1 # in the paper they use 1
+            alpha = 1 # In the paper they use 1
             loss +=  alpha * criterion(outputs[1], targets) + alpha *  criterion(outputs[2], targets)
              
 
@@ -77,12 +100,11 @@ def train(epoch, old_model, dataloader_train, criterion, optimizer, iter, learni
         optimizer.step()
 
         # Compute the learning rate
-        lr = poly_lr_scheduler(optimizer, init_lr=learning_rate, iter=iter, lr_decay_iter=1, max_iter=max_iter, power=0.9)
+        lr = poly_lr_scheduler(optimizer, init_lr=learning_rate, iter=iteration, lr_decay_iter=1, max_iter=max_iter, power=0.9)
 
         # Update the running loss
-        running_loss += loss.item() # Update of the loss = contain the total loss of the epoch
+        running_loss += loss.item() # Update of the loss == contain the total loss of the epoch
 
-        ## Chat gpt dice: ##
         # Convert model outputs to predicted class labels
         preds = outputs[0].argmax(dim=1).detach().cpu().numpy()
         gts = targets.detach().cpu().numpy()
@@ -93,24 +115,30 @@ def train(epoch, old_model, dataloader_train, criterion, optimizer, iter, learni
         total_unions += unions
 
     # 5. Compute the metrics for the training set 
-    # 5.a Compute the accuracy metrics, i.e. mIoU and mean loss
+    # 5.a Compute the standard metrics for all the epochs
     print("Computing the metrics for the training set...")
+
     iou_per_class = (total_intersections / (total_unions + 1e-10)) * 100
     iou_non_zero = np.array(iou_per_class)
     iou_non_zero = iou_non_zero[np.nonzero(iou_non_zero)]
-    # Calcola la media ignorando i NaN
+
+    # Compute the mean without considering NaN value
     mean_iou = np.nanmean(iou_non_zero) 
     mean_loss = running_loss / len(dataloader_train)    
 
     # 5.b Compute the computation metrics, i.e. FLOPs, latency, number of parameters (only at the last epoch)
     if epoch == 50:
             print("Computing the computation metrics...")
+
             mean_latency, std_latency, mean_fps, std_fps = compute_latency_and_fps(model, height=512, width=1024, iterations=1000)
             print(f"Latency: {mean_latency:.2f} ± {std_latency:.2f} ms | FPS: {mean_fps:.2f} ± {std_fps:.2f}")
+
             num_flops = compute_flops(model, height=512, width=1024)
             print(f"Total numer of FLOPS: {num_flops} GigaFLOPs")
+
             tot_params, trainable_params = compute_parameters(model)
             print(f"Total Params: {tot_params}, Trainable: {trainable_params}")
+
     else:
         # NB: metric = -1 means we have not computed it (we compute only at the last epoch)
         mean_latency = -1
@@ -122,16 +150,16 @@ def train(epoch, old_model, dataloader_train, criterion, optimizer, iter, learni
 
 
 
-    # 6. SAVE THE PARAMETERS OF THE MODEL 
+    # 6. Save the parameter of the model 
     print("Saving the model")
-    # Loggare la loss e altri dati per wandb
+
     wandb.log({
         "epoch": epoch,
         "loss": mean_loss,
         "lr": lr
     })
 
-    # Salva i pesi del modello dopo ogni epoca
+    # Save the model weight at each epoch
     model_save_path = f"model_epoch_{epoch}.pt"
     torch.save({
         'epoch': epoch,
@@ -140,20 +168,13 @@ def train(epoch, old_model, dataloader_train, criterion, optimizer, iter, learni
         'loss': mean_loss,
     }, model_save_path)
 
-    # Crea un nuovo artifact per il modello della epoca corrente
+    # Create a new artefact for the current epoch
     artifact = wandb.Artifact(f"model_epoch_{epoch}", type="model")
-    artifact.add_file(model_save_path)  # Aggiungi il file del modello all'artifact
+    artifact.add_file(model_save_path) 
 
-    # Registra l'artifact su W&B
+    # Store the artefact on wandb
     wandb.log_artifact(artifact)
     print(f"Model saved for epoch {epoch}")
-    """
-    # Salva il modello su wandb
-    wandb.save(model_save_path)
-
-    # Alla fine del ciclo, termina il run di wandb
-    wandb.finish()
-    print("Model saved")"""
 
     # 7. Return all the metrics
     metrics = {
@@ -167,4 +188,203 @@ def train(epoch, old_model, dataloader_train, criterion, optimizer, iter, learni
         'num_flops' : num_flops,
         'trainable_params': trainable_params
     }
-    return metrics, iter
+
+    return metrics, iteration
+
+
+def train_with_adversary(epoch, old_model, discriminators, dataloader_source_train, dataloader_target_train, 
+                         criterion, optimizer, discriminator_optimizers, iteration, learning_rate, num_classes, max_iter, lambdas): # criterion == loss function
+   
+    # --------------------------- BASIC DEFINITIONS -------------------------------------- #
+    try:
+        var_model = os.environ['MODEL'] 
+    except KeyError:
+        print("Environment variable 'MODEL' not set. Using default model 'BiSeNet'.")
+        var_model = "BiSeNet"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = old_model.to(device) 
+    running_loss = 0.0 
+    mean_loss = 0.0
+    total_intersections = np.zeros(num_classes)
+    total_unions = np.zeros(num_classes)
+    target_label = 0
+    source_label = 1
+    target_iter = iter(dataloader_target_train) # Create an iterator for the target dataset
+
+
+    model.train() 
+    # --------------------------- TRAINING LOOP -------------------------------------- #
+    for batch_idx, (inputs_src, targets_src, file_names) in enumerate(dataloader_source_train): 
+        if batch_idx % 100 == 0: # Print every 100 batches
+            print(f"Batch {batch_idx}/{len(dataloader_source_train)}")
+
+        # ------------------- TRAINING BISENET WITH ADVERSARIAL LOSS ------------------- #
+        for discriminator in discriminators:
+            lock_model(discriminator.to(device)) # Lock the discriminator parameters to avoid training them
+
+
+        iteration += 1 # Increment the iteration counter
+        inputs_src, targets_src = inputs_src.to(device), targets_src.to(device) # GPU
+
+        # Compute output of the train
+        outputs = model(inputs_src)        
+        loss = criterion(outputs[0], targets_src)
+        
+        alpha = 1 # In the paper they use 1
+        loss +=  alpha * criterion(outputs[1], targets_src) + alpha *  criterion(outputs[2], targets_src)
+
+        # Get the next batch from the target dataset   
+        try:
+            inputs_target, _, _ = next(target_iter)
+        except StopIteration:
+            target_iter = iter(dataloader_target_train)
+            inputs_target, _, _ = next(target_iter)
+        inputs_target = inputs_target.to(device)
+
+        # Compute the output of the target dataset
+        outputs_target = model(inputs_target)
+
+        # Compute the adversarial loss
+        adv_loss = adversarial_loss(discriminators, outputs_target, target_label, source_label, device, lambdas)
+        # Combine the losses
+        loss += adv_loss
+
+        # Backpropagation
+        optimizer = backpropagate(optimizer, loss)
+
+        # Compute the learning rate
+        lr = poly_lr_scheduler(optimizer, init_lr=learning_rate, iter=iteration, lr_decay_iter=1, max_iter=max_iter, power=0.9)
+
+        # Update the running loss
+        running_loss += loss.item() # Update of the loss == contain the total loss of the epoch
+
+        # Convert model outputs to predicted class labels
+
+        # ------------------- TRAINING DISCRIMINATORS ------------------- #
+        lock_model(model) # Lock the model parameters to avoid training them
+        for discriminator in discriminators:
+            unlock_model(discriminator)
+        
+        with torch.no_grad():
+            outputs_source = model(inputs_src)
+            outputs_target = model(inputs_target)
+        
+        softmax_src = softmax(outputs_source[0], dim=1).detach()
+        softmax_tgt = softmax(outputs_target[0], dim=1).detach()
+
+        for i, discriminator in enumerate(discriminators):
+            disc_optimizer = discriminator_optimizers[i]
+            disc_optimizer.zero_grad()
+
+            # SOURCE: discriminator deve dire "source_label"
+            disc_pred_src = discriminator(softmax_src)
+            loss_d_src = torch.nn.functional.binary_cross_entropy_with_logits(
+                disc_pred_src,
+                torch.full(disc_pred_src.shape, float(source_label), device=device)
+            )
+
+            # TARGET: discriminator deve dire "target_label"
+            disc_pred_tgt = discriminator(softmax_tgt)
+            loss_d_tgt = torch.nn.functional.binary_cross_entropy_with_logits(
+                disc_pred_tgt,
+                torch.full(disc_pred_tgt.shape, float(target_label), device=device)
+            )
+
+            # Media delle due loss
+            loss_d = 0.5 * (loss_d_src + loss_d_tgt)
+            loss_d.backward()
+            disc_optimizer.step()
+
+        unlock_model(model) # Unlock the model parameters to allow training
+
+        preds = outputs[0].argmax(dim=1).detach().cpu().numpy()
+        gts = targets_src.detach().cpu().numpy()
+
+        # Accumulate intersections and unions per class
+        _, _, inters, unions = compute_miou(gts, preds, num_classes)
+        total_intersections += inters
+        total_unions += unions
+
+        del outputs, outputs_target, softmax_src, softmax_tgt, preds
+        torch.cuda.empty_cache()
+        gc.collect()
+
+        
+    # --------------------------- END OF TRAINING LOOP -------------------------------------- #
+
+    # 5. Compute the metrics for the training set 
+    # 5.a Compute the standard metrics for all the epochs
+    print("Computing the metrics for the training set...")
+
+    iou_per_class = (total_intersections / (total_unions + 1e-10)) * 100
+    iou_non_zero = np.array(iou_per_class)
+    iou_non_zero = iou_non_zero[np.nonzero(iou_non_zero)]
+
+    # Compute the mean without considering NaN value
+    mean_iou = np.nanmean(iou_non_zero) 
+    mean_loss = running_loss / len(dataloader_source_train)    
+
+    # 5.b Compute the computation metrics, i.e. FLOPs, latency, number of parameters (only at the last epoch)
+    if epoch == 50:
+            print("Computing the computation metrics...")
+
+            mean_latency, std_latency, mean_fps, std_fps = compute_latency_and_fps(model, height=512, width=1024, iterations=1000)
+            print(f"Latency: {mean_latency:.2f} ± {std_latency:.2f} ms | FPS: {mean_fps:.2f} ± {std_fps:.2f}")
+
+            num_flops = compute_flops(model, height=512, width=1024)
+            print(f"Total numer of FLOPS: {num_flops} GigaFLOPs")
+
+            tot_params, trainable_params = compute_parameters(model)
+            print(f"Total Params: {tot_params}, Trainable: {trainable_params}")
+
+    else:
+        # NB: metric = -1 means we have not computed it (we compute only at the last epoch)
+        mean_latency = -1
+        std_latency = -1
+        num_flops = -1
+        trainable_params = -1
+        mean_fps = -1
+        std_fps = -1
+
+
+
+    # 6. Save the parameter of the model 
+    print("Saving the model")
+
+    wandb.log({
+        "epoch": epoch,
+        "loss": mean_loss,
+        "lr": lr
+    })
+
+    # Save the model weight at each epoch
+    model_save_path = f"model_epoch_{epoch}.pt"
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': mean_loss,
+    }, model_save_path)
+
+    # Create a new artefact for the current epoch
+    artifact = wandb.Artifact(f"model_epoch_{epoch}", type="model")
+    artifact.add_file(model_save_path) 
+
+    # Store the artefact on wandb
+    wandb.log_artifact(artifact)
+    print(f"Model saved for epoch {epoch}")
+
+    # 7. Return all the metrics
+    metrics = {
+        'mean_loss': mean_loss,
+        'mean_iou': mean_iou,
+        'iou_per_class': iou_per_class,
+        'mean_latency' : mean_latency,
+        'std_latency' : std_latency,
+        'mean_fps' : mean_fps,
+        'std_fps' : std_fps,
+        'num_flops' : num_flops,
+        'trainable_params': trainable_params
+    }
+
+    return metrics, iteration
