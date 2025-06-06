0efdd59cc806a142a5a6e2ba87ea43072164abd2
Aurona Gashi <s281401@studenti.polito.it>
2025-06-06 10:04:32 +0200

    start epoch 17

--- main4_train.py
+++ main4_train.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     num_epochs = 50 
     num_classes = 19
     ignore_index = 255
-    start_epoch = 1 #CHECK BEFORE RUNNING
+    start_epoch = 17 #CHECK BEFORE RUNNING
 
     # Transformation
     transform_gta_dataset = transform_gta()
