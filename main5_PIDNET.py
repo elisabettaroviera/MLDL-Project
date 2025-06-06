07d9511242e84c42538d17061d82ca22dd057b22
Aurona Gashi <s281401@studenti.polito.it>
2025-06-06 15:00:48 +0200

    forse ce la famo, ce 1 con pretarined tot datasaset

--- main5_PIDNET.py
+++ main5_PIDNET.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     print("Load the datasets")
     cs_train = CityScapes('/kaggle/input/cityscapes-dataset/Cityscapes', 'train', transform, target_transform)
     cs_val = CityScapes('/kaggle/input/cityscapes-dataset/Cityscapes', 'val', transform, target_transform)
-    
+
     class CFG:
         pass
 
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     
     # Defintion of the loss function: usano cross entropy nel apper
     print("Definition of the loss")
-    loss = CombinedLoss_All(num_classes=num_classes, alpha=0.7, beta=0, gamma=0.3, theta=0, ignore_index=255) # CHANGE HERE THE LOSS
+    loss = CombinedLoss_All(num_classes=num_classes, alpha=1.0, beta=0, gamma=0, theta=0, ignore_index=255) # CHANGE HERE THE LOSS
     # in realta in train e validation la ridefinisco 
     # alpha   - CrossEntropy
     # beta    - Lovász
@@ -170,7 +170,7 @@ if __name__ == "__main__":
         # To save the model we need to initialize wandb 
         # entity="s328422-politecnico-di-torino" # Old entity Betta
         entity = "s281401-politecnico-di-torino" # New entity  Auro
-        project_name = f"5_PIDNET_07ce_03tv_lr_0.00625_totloss_100_percent"
+        project_name = f"5_PIDNET_1ce_0.00625_totloss_100_percent"
         wandb.init(project=project_name, entity=entity, name=f"epoch_{epoch}", reinit=True) 
         print("Wandb initialized")
 
