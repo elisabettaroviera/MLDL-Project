92d3bcac70db27830c47c7ea6203be1e15115473
LuciaGhezzi <s325951@studenti.polito.it>
2025-06-24 09:59:50 +0200

    train correct one

--- main4_train.py
+++ main4_train.py
@@ -197,10 +197,10 @@ if __name__ == "__main__":
 
 
     # === Step 1: Add global config for trials in the main training script ===
-    trial_type = "hinge_fixed"  # Options: bce_fixed (base), hinge_rampup, mse_rampup, bce_confidence,  #NB add hinge_fixed
-    lambdas = [0.002]  # Lambda values for the adversarial loss, only one for the single discriminator
+    trial_type = "hinge_rampup"  # Options: bce_fixed (base), hinge_rampup, mse_rampup, bce_confidence,  #NB add hinge_fixed
+    lambdas = [0.001]  # Lambda values for the adversarial loss, only one for the single discriminator
      
-    project_name = "4_Adversarial_Domain_Adaptation_hinge_fixed_0002_augmented" #CHECK BEFORE RUNNING
+    project_name = "4_Adversarial_Domain_Adaptation_hinge_rampup_augmented" #CHECK BEFORE RUNNING
     entity = "s281401-politecnico-di-torino" # New new entity Auro
     # entity = "s325951-politecnico-di-torino-mldl" # new team Lucia
     # entity="s328422-politecnico-di-torino" # old team Betta
