717eddbf6059313ac89da8e3dccb673b10e57c6e
etta1509 <s328422@studenti.polito.it>
2025-06-28 16:44:38 +0200

    indent sbagliata -.-

--- datasets/gta5.py
+++ datasets/gta5.py
@@ -52,7 +52,26 @@ class GTA5(Dataset):
             # Return total number of samples
             return len(self.images)
 
+    def __getitem__(self, idx):
+        image_path = self.images[idx]
+        image = Image.open(image_path).convert('RGB')
+        label = Image.open(self.masks[idx])
 
+        if self.augmentation:
+            # Apply the Augmentation to all the image
+            augmented = augmentation_transform(image=np.array(image), mask=np.array(label), type_aug = self.type_aug)
+            image = Image.fromarray(augmented['image'])
+            label = Image.fromarray(augmented['mask'])
+
+        # Apply standard transformation such as resize
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        filename = os.path.basename(image_path)
+        return image, label, filename
+    
     # get item dentro gta5 PER LA BEST AUGM DI LUCI
     """
     def __getitem__(self, idx):
@@ -77,22 +96,4 @@ class GTA5(Dataset):
         filename = os.path.basename(image_path)
         return image, label, filename
     """
-     def __getitem__(self, idx):
-        image_path = self.images[idx]
-        image = Image.open(image_path).convert('RGB')
-        label = Image.open(self.masks[idx])
-
-        if self.augmentation:
-            # Apply the Augmentation to all the image
-            augmented = augmentation_transform(image=np.array(image), mask=np.array(label), type_aug = self.type_aug)
-            image = Image.fromarray(augmented['image'])
-            label = Image.fromarray(augmented['mask'])
-
-        # Apply standard transformation such as resize
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-
-        filename = os.path.basename(image_path)
-        return image, label, filename
+     
